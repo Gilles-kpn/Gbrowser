daaae6396292760cea42db156757980b9ff31d7d
Kpanou Gilles <76532932+Gilles-kpn@users.noreply.github.com>
2021-07-20 06:50:17 +0100

    Add files via upload

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module Web.Browser {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.web;
+    requires javafx.graphics;
+    requires javafx.media;
+    opens sample;
+}
